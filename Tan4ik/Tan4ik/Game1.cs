6b672f787e9f77c45dc1850c3967564dc2b22278
SkyForce <nikestrike0@gmail.com>
2012-01-11 13:53:23 +0700

    added license

--- Tan4ik/Tan4ik/Game1.cs
+++ Tan4ik/Tan4ik/Game1.cs
@@ -1,10 +1,9 @@
 ﻿/* Copyright (C) 2012 by SkyForce (twice@eml.ru)
- * Это произведение распространяется по лицензии 
- * Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported. 
- * Чтобы ознакомиться с экземпляром этой лицензии, 
- * посетите http://creativecommons.org/licenses/by-nc-nd/3.0/
- * или отправьте письмо на адрес Creative Commons: 
- * 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+ * This work is licensed under the 
+ * Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
+ * To view a copy of this license, visit 
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to Creative Commons, 
+ * 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 */
 
 using System;
