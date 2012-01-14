530a27fff51bf2bf887b80db636d6d35235a888a
SkyForce <nikestrike0@gmail.com>
2012-01-14 18:28:15 +0700

    added new features

--- Tan4ik/Tan4ik/Shell.cs
+++ Tan4ik/Tan4ik/Shell.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/* Copyright (C) 2012 by SkyForce (twice@eml.ru)
+ * This work is licensed under the 
+ * Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
+ * To view a copy of this license, visit 
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to Creative Commons, 
+ * 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
